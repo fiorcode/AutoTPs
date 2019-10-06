6e622685f63b9a54fbffc9ec2e4ab44ea8ab935c
fiorcode <45425686+fiorcode@users.noreply.github.com>
2019-10-06 15:26:57 -0300

    Update Question.cs

--- Question.cs
+++ Question.cs
@@ -6,11 +6,22 @@ namespace AutoTPs
 {
     class Question
     {
+        public Question()
+        {
+            Answers = new List<string>();
+            WrongAnswers = new List<string>();
+            CorrectAnswers = new List<string>();
+        }
+
         public string Id { get; set; }
+
+        public string Type { get; set; }
 
         public bool Resolved { get; set; }
         
         public List<string> Answers { get; set; }
+
+        public List<string> WrongAnswers { get; set; }
 
         public List<string> CorrectAnswers { get; set; }
 
