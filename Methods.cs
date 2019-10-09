8a854b8c5b932f3f1561797b99a908ac58f7b73a
fiorcode <45425686+fiorcode@users.noreply.github.com>
2019-10-09 12:20:07 -0300

    Create Methods.cs

---
+++ Methods.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+
+namespace AutoTPs
+{
+    class Methods
+    {
+        public static void EnterText(
+            string element,
+            string value,
+            string elementType)
+        {
+            if (elementType.ToLower() == "Id") Driver.GetInstance.WebDrive.FindElement(By.Id(element)).SendKeys(value);
+            if (elementType == "Name") Driver.GetInstance.WebDrive.FindElement(By.Name(element)).SendKeys(value);
+        }
+
+        public static void Click(
+            string element,
+            string elementType)
+        {
+            if (elementType == "Id") Driver.GetInstance.WebDrive.FindElement(By.Id(element)).Click();
+            if (elementType == "Name") Driver.GetInstance.WebDrive.FindElement(By.Name(element)).Click();
+            if (elementType == "ClassName") Driver.GetInstance.WebDrive.FindElement(By.ClassName(element)).Click();
+            if (elementType == "HRef") Driver.GetInstance.WebDrive.FindElement(By.CssSelector(element)).Click();
+        }
+    }
+}
