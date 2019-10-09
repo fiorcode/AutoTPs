132692795721d7033dbd555ec77d65a59301590d
fiorcode <45425686+fiorcode@users.noreply.github.com>
2019-10-09 12:20:04 -0300

    Create Driver.cs

---
+++ Driver.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace AutoTPs
+{
+    public sealed class Driver
+    {
+        private static Driver instance = null;
+
+        private Driver(IWebDriver driver)
+        {
+            this.WebDrive = driver;
+        }
+
+        public static Driver GetInstance
+        {
+            get
+            {
+                if (instance == null) instance = new Driver(new ChromeDriver("."));
+                return instance;
+            }
+        }
+
+        public IWebDriver WebDrive { get; }
+
+    }
+}
