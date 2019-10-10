922f49b23bcaab640523715e2ef76f0c93336262
fiorcode <45425686+fiorcode@users.noreply.github.com>
2019-10-10 15:23:10 -0300

    Create TP.cs

---
+++ TP.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoTPs
+{
+    class TP
+    {
+        public TP()
+        {
+            Questions = new List<Question>();
+            CurrentQuestions = new List<Question>();
+            LastMark = 0;
+            CurrentExpectedMark = 0;
+        }
+
+        public string Id { get; set; }
+
+        public List<Question> Questions { get; set; }
+
+        public List<Question> CurrentQuestions { get; set; }
+
+        public double LastMark { get; set; }
+
+        public double CurrentExpectedMark { get; set; }
+
+    }
+}
