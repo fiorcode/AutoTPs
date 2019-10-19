0aea4d9695c40fe5db6e1348011eccfa617f1f4e
fiorcode <45425686+fiorcode@users.noreply.github.com>
2019-10-19 20:52:36 -0300

    Update Methods.cs

--- Methods.cs
+++ Methods.cs
@@ -1,4 +1,5 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 
 namespace AutoTPs
 {
@@ -21,6 +22,21 @@ namespace AutoTPs
             if (elementType == "Name") Driver.GetInstance.WebDrive.FindElement(By.Name(element)).Click();
             if (elementType == "ClassName") Driver.GetInstance.WebDrive.FindElement(By.ClassName(element)).Click();
             if (elementType == "HRef") Driver.GetInstance.WebDrive.FindElement(By.CssSelector(element)).Click();
+        }
+
+        public static void SelectDropDown(
+            string element,
+            string elementType,
+            string value)
+        {
+            if (elementType == "Id")
+                new SelectElement(Driver.GetInstance.WebDrive.FindElement(By.Id(element))).SelectByValue(value);
+            if (elementType == "Name")
+                new SelectElement(Driver.GetInstance.WebDrive.FindElement(By.Name(element))).SelectByValue(value);
+            if (elementType == "ClassName")
+                new SelectElement(Driver.GetInstance.WebDrive.FindElement(By.ClassName(element))).SelectByValue(value);
+            if (elementType == "HRef")
+                new SelectElement(Driver.GetInstance.WebDrive.FindElement(By.CssSelector(element))).SelectByValue(value);
         }
     }
 }
